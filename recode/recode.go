933533851c6c7b68755bf7a9688d36b4ab492b85
Mente <dev@imkreative.com>
2024-07-21 00:42:41 -0400

    setup better 2pass system. Add progress channel

--- recode/recode.go
+++ recode/recode.go
@@ -1,13 +1,25 @@
 package recode
 
 import (
-	"context"
-	"log"
-	"os"
-	"sync"
+    "context"
+    "log"
+    "os"
 
-	"github.com/xfrr/goffmpeg/v2/ffmpeg"
+    "github.com/xfrr/goffmpeg/v2/ffmpeg"
+    "github.com/xfrr/goffmpeg/v2/ffmpeg/progress"
+    "github.com/xfrr/goffmpeg/v2/ffprobe"
+    "github.com/xfrr/goffmpeg/v2/pkg/media"
 )
+
+type passdata struct {
+    firstPass *ffmpeg.Command
+    secondPass *ffmpeg.Command
+    firstPassChannel chan bool
+    secondPassChannel chan bool
+    progressChannel chan float64
+    doneChannel chan struct{}
+    fileInfo media.File
+}
 
 func Encode(in string, out string) error {
     _, err := os.Create("/dev/null")
@@ -16,12 +28,8 @@ func Encode(in string, out string) error {
         return err
     }
 
-    var wg sync.WaitGroup
-
     ch1 := make(chan bool)
     ch2 := make(chan bool)
-    defer close(ch1)
-    defer close(ch2)
 
     c1 := ffmpeg.NewCommand().
         WithInputPath(in).
@@ -46,57 +54,129 @@ func Encode(in string, out string) error {
         WithThreadAmount(16).
         WithOutputPath(out)
 
-    go pass(c1, ch1, ch2, &wg)
-    go pass(c2, ch2, nil, &wg)
+    pd, err := newPassData(c1, c2, ch1, ch2, in)
+
+    if err != nil {
+        return err
+    }
+
+    go pass(pd, nil)
+    go pass(pd, pd.secondPass)
 
     ch1 <- true
 
-    wg.Wait()
-
-    os.Remove("fil.log")
-
-    return nil
+    for {
+        select {
+        case progress := <-pd.progressChannel:
+            log.Printf("Progress: %.2f\n", progress)
+        case <-pd.doneChannel:
+            os.Remove("fil.log")
+            return nil
+    }
+    }
 }
 
-func pass (c *ffmpeg.Command, start <-chan bool, stop chan<- bool, wg *sync.WaitGroup) {
-    wg.Add(1)
+func newPassData(firstPass, secondPass *ffmpeg.Command, ch1, ch2 chan bool, inputPath string) (*passdata, error) {
+    p := make(chan float64)
+    d := make(chan struct{})
 
-    log.Println("Added")
     ctx := context.Background()
-    ctx, cancel1 := context.WithCancel(ctx)
-    defer cancel1()
+    ctx, cancel := context.WithCancel(ctx)
+    defer cancel()
+
+    f, err := ffprobe.NewCommand().WithInputPath(inputPath).Run(ctx)
+
+    if err != nil {
+        return nil, err
+    }
+
+    return &passdata{
+        firstPass: firstPass,
+        secondPass: secondPass,
+        firstPassChannel: ch1,
+        secondPassChannel: ch2,
+        progressChannel: p,
+        doneChannel: d,
+        fileInfo: f,
+    }, nil
+}
+
+func pass (d *passdata, command *ffmpeg.Command) {
+    ctx := context.Background()
+    ctx, cancel := context.WithCancel(ctx)
+    defer cancel()
+
+    isSecondPass := false
+
+    if command != nil {
+        isSecondPass = true
+    }
+
+    pc := make(chan struct{}, 1)
 
     go func() {
-        log.Println("Starting")
-        log.Println(c)
+        var p <-chan progress.Progress
+        var err error
 
-        <-start
+        if !isSecondPass {
+            <-d.firstPassChannel
+            p, err = d.firstPass.Start(ctx)
+        } else {
+            <-d.secondPassChannel
+            p, err = d.secondPass.Start(ctx)
+        }
 
-        p, err := c.Start(ctx)
 
         if err != nil {
             panic(err)
         }
 
         go func() {
-            for msg := range p {
-                log.Printf("%2.f", msg.Duration().Seconds())
+            for {
+                select {
+                case <-pc:
+                    if !isSecondPass {
+                        close(d.firstPassChannel)
+                    } else {
+                        close(d.secondPassChannel)
+                    }
+
+                    close(pc)
+                    return
+                default:
+                    for msg := range p {
+                        if !isSecondPass {
+                            d.progressChannel <-float64(msg.Duration().Milliseconds()) / (float64(d.fileInfo.Duration().Milliseconds()) * 2) * 100
+                        } else {
+                            d.progressChannel <-float64(msg.Duration().Milliseconds()) / (float64(d.fileInfo.Duration().Milliseconds()) * 2) * 100 + 50.0
+                        }
+
+                    }
+            }
             }
         }()
 
     }()
 
-    err := c.Wait()
+    var err error
+
+    if !isSecondPass {
+        err = d.firstPass.Wait()
+    } else {
+        err = d.secondPass.Wait()
+    }
 
     if err != nil {
         panic(err)
     }
 
-    wg.Done()
+    pc <- struct{}{}
 
-    if stop != nil {
-        stop <-true
+    if !isSecondPass {
+        d.secondPassChannel  <- true
+    } else {
+        d.doneChannel <- struct{}{}
+        close(d.doneChannel)
+        close(d.progressChannel)
     }
-
-    log.Println("Done")
 }
